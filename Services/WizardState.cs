f0d0cf3fa10fcd69c85a643519da16982c54cb39
AMAURY <amaury.vanrossom@student.hogent.be>
2025-12-06 17:13:48 +0100

    Created main layout as base for UI of all steps

--- Services/WizardState.cs
+++ Services/WizardState.cs
@@ -5,12 +5,23 @@ namespace excel_workflow.Services
     public class WizardState
     {
         private WizardModel _wizardModel;
+        private int _step;
+        private bool[] _stepCompleted;
 
         public WizardState()
         {
             _wizardModel = new WizardModel();
+            _step = 0;
+            _stepCompleted = new bool[12];
         }
 
         public WizardModel WizardModel { get => _wizardModel; set => _wizardModel = value; }
+        public int Step { get => _step; set => _step = value; }
+        public bool[] StepCompleted { get => _stepCompleted; set => _stepCompleted = value; }
+
+        public void GoToNextStep()
+        {
+            Step++;
+        }
     }
 }
