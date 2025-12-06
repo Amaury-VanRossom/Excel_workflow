3aef84bd6bb0f049e1dc2361d471432c13e73be7
AMAURY <amaury.vanrossom@student.hogent.be>
2025-12-06 15:55:35 +0100

    Added WizardState to use WizardModel in Razor pages

---
+++ Services/WizardState.cs
@@ -0,0 +1,16 @@
+﻿using excel_workflow.Models;
+
+namespace excel_workflow.Services
+{
+    public class WizardState
+    {
+        private WizardModel _wizardModel;
+
+        public WizardState()
+        {
+            _wizardModel = new WizardModel();
+        }
+
+        public WizardModel WizardModel { get => _wizardModel; set => _wizardModel = value; }
+    }
+}
