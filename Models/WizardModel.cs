ea88e968c23c5f93f6ea7209afdd23492112e0ae
AMAURY <amaury.vanrossom@student.hogent.be>
2025-12-13 15:11:01 +0100

    Removed Step 8 and RoomChoices. Step 9 is now step 8

--- Models/WizardModel.cs
+++ Models/WizardModel.cs
@@ -12,7 +12,6 @@ namespace excel_workflow.Models
         private Dictionary<int, Student> _students;
         private List<ExamRoom> _rooms;
         //lokaal kiezen voor Gent en Aalst voor "ander te selecteren lokaal" en TIAO/VC
-        private RoomChoices? _roomChoices;
         private Dictionary<Student, ExamRoom> _assignedStudents;
         public string? Olod { get => _olod; set => _olod = value; }
         public ExamType? ExamType { get => _examType; set => _examType = value; }
@@ -20,7 +19,6 @@ namespace excel_workflow.Models
         public Dictionary<Measure, MeasureTaken> MeasuresTaken { get => _measuresTaken; set => _measuresTaken = value; }
         public Dictionary<int, Student> Students { get => _students; set => _students = value; }
         public List<ExamRoom> Rooms { get => _rooms; set => _rooms = value; }
-        public RoomChoices? RoomChoices { get => _roomChoices; set => _roomChoices = value; }
         public Dictionary<Student, ExamRoom> AssignedStudents { get => _assignedStudents; set => _assignedStudents = value; }
 
         public WizardModel()
@@ -29,27 +27,6 @@ namespace excel_workflow.Models
             _students = new Dictionary<int, Student>();
             _rooms = new List<ExamRoom>();
             _assignedStudents = new Dictionary<Student, ExamRoom>();
-        }
-
-        public IEnumerable<Student> GetStudentsFromCity(City city)
-        {
-            return Students.Values
-                .Where(s => {
-                    Olod? olod = s.Olods.FirstOrDefault(o => o.Name == Olod);
-                    return olod is not null && !olod.Exemption && (city == City.Aalst 
-                    ? (olod.Traject.Equals(Traject.Aalst) || (olod.VCCity?.Equals(City.Aalst) ?? false)) 
-                    : (!olod.Traject.Equals(Traject.Aalst)) || (olod.VCCity?.Equals(City.Gent) ?? false));
-                });
-
-        }
-
-        public IEnumerable<(Student, Olod)> GetStudentsWithOlod(string olodName)
-        {
-            Console.WriteLine(olodName);
-            var values = Students.Values.Select(s => (s, s.Olods.FirstOrDefault(o => o.Name.Equals(olodName))))
-                .Where(t => t.Item2 is not null).Cast<(Student, Olod)>();
-            Console.WriteLine(values.Count());
-            return values;
         }
 
         public bool NeedRoom(Student student, MeasureTaken measureTaken)
